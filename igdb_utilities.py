dbf7556b8c1fc1856df4c617b13ba2201175782d
Drilon Pollozhani <drilonpollozhani@Drilons-MBP.lan>
2021-02-25 17:43:12 +0100

    Update igdb_utilities.py

--- igdb_utilities.py
+++ igdb_utilities.py
@@ -1,11 +1,3 @@
-# from igdb_authentication import get_token
-# import os
-# import sys
-# import inspect
-# import json
-# from ast import literal_eval
-# from igdb.wrapper import IGDBWrapper
-
 rating_enum = {1: 'Three', 
     2: 'Seven',
     3: 'Twelve',
@@ -149,37 +141,3 @@ def clean_company_info(info):
         else:
             clean_info[key] = value
     return clean_info
-
-# def get_data(endpoint:str, query:str):
-#     wrapper = IGDBWrapper(os.environ.get('TWITCH_ID'), get_token())
-
-#     byte_array = wrapper.api_request(
-#                 endpoint,
-#                 query 
-#                 )
-#     try:
-#         data = literal_eval(byte_array.decode('utf-8'))
-#         exception_occurred = False
-#     except Exception as e:
-#         print('==================')
-#         print('Warning: literal evaluation error in database query:', e, ': trying to load data as json file')
-#         print('Module/Function : ' + os.path.basename(__file__) + ' ' + sys._getframe().f_code.co_name +'()') 
-#         print('Called from     : ' + os.path.basename(inspect.stack()[1][1]) +' ' + inspect.stack()[1][3] + '()')
-#         exception_occurred = True
-#     if exception_occurred:
-#         data = json.loads(byte_array)
-
-#     return data
-
-# def to_json(data, outfile, indent=3):
-#     if not '.json' in outfile:
-#         outfile += '.json'
-#     with open(outfile, 'w') as f:
-#         json.dump(data, f, indent=indent)
-#         f.close()
-
-# def open_json(infile):
-#     with open(infile, 'r') as f:
-#         data = json.load(f)
-#         f.close()
-#     return data
